ded168793835f3a009610120ad1143a8f7b143ab
kakk2093 <80924040+kakk2093@users.noreply.github.com>
2023-01-31 11:23:53 +0400

    fix fx

--- Assets/_Dev/FxPool/Scripts/DefaultFx.cs
+++ Assets/_Dev/FxPool/Scripts/DefaultFx.cs
@@ -1,8 +1,36 @@
+using System.Collections;
 using UnityEngine;
 using Zenject;
 
 public class DefaultFx : MonoBehaviour
 {
+    [SerializeField] private float _despownTime = 2f;
+
+    private DefaultFxPool _pool;
+
+    [Inject]
+    private void Construct(DefaultFxPool defaultFxPool)
+    {
+        _pool = defaultFxPool;
+    }
+
+    private void OnEnable()
+    {
+        DespawnUnit();
+    }
+
+    private void DespawnUnit()
+    {
+        StartCoroutine(DespawnCorutine());
+    }
+
+    private IEnumerator DespawnCorutine()
+    {
+        yield return new WaitForSeconds(_despownTime);
+        _pool.Despawn(this);
+    }
+
+
     public class DefaultFxPool : MemoryPool<DefaultFx>
     {
         protected override void OnCreated(DefaultFx unit)
